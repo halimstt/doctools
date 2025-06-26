0e87d619648a997e1739eeac17861e9f765d9d53
Abd Halim <halimstt@gmail.com>
2025-06-27 00:57:24 +0800

    Fix: Vite add resolve point

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Define entry points for your HTML files
+        main: resolve(__dirname, "index.html"),
+        invoice: resolve(__dirname, "invoice.html"),
+        // Add more HTML entry points here if you have them
+      },
+    },
+    // Specify the output directory for the build (default is 'dist')
+    outDir: "docs",
+    // Clear the output directory before building
+    emptyOutDir: true,
+  },
 });
